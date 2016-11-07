c7c4c03af6bbf5737a187692d01e3d09377f582e
ahilasaravanan <ahila@falconnect.in>
2016-11-07 10:19:23 +0530

    registration route changes

--- dmrprod/app/Http/routes.php
+++ dmrprod/app/Http/routes.php
@@ -22,3 +22,27 @@ Route::get('delete/{id}','car_managelisting@delete_car_listing');
 Route::get('view/{id}','car_managelisting@view_car_listing');
 Route::get('update/{id}','car_managelisting@update_car_listing');
 Route::post('edit_car_listing','car_managelisting@edit_car_listing');
+
+Route::get('/', function () {
+
+    return view('registration');
+
+});
+
+Route::get('/registration', function () {
+    return view('registration');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('create','registercontroller@create');
+Route::post('store','registercontroller@store');
+
+Route::get('mailsend/{id}',array('as'=>'mailsend','uses'=>'registercontroller@mailsend'));
+
+//Route::get('mailsend/{id}',function())
+
+Route::get('passwordactivation/{id}',array('as'=>'passwordactivation','uses'=>'registercontroller@passwordconfirm'));
+Route::post('passwordconfirm','registercontroller@passwordconfirm');
