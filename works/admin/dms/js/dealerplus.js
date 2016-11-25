f0e2517f790624a248163115372292ee96648462
NandhiniAshokkumar <nandhini@falconnect.in>
2016-11-25 18:08:24 +0530

    file changed

--- works/admin/dms/js/dealerplus.js
+++ works/admin/dms/js/dealerplus.js
@@ -5,29 +5,33 @@ $(function () {
 
 
 /*Field Set Cloning on clicking add button. Button class should be defined as cloneAdd inside the node that has to cloned */
-
+var cloneCounter = 0;
 $(document).on('click', '.cloneAdd', function ($this) {
-    var cloneCounter = 0;
     cloneCounter++;
     var oCloneId = $(this).parent().attr('id');
     var oClone = document.getElementById(oCloneId + 'Clones').cloneNode(true);
-    //var oCloneChildren = oClone.children;
     oClone.id = (oCloneId + 'Rows');
 //    oClone.className = (oCloneId + 'C')
-    //document.getElementById("placeholder").appendChild(oClone);
     var oCloneParent = $(this).parent().attr('id');
     document.getElementById(oCloneId).appendChild(oClone);
+    if ($('#' + oCloneId + 'Rows').length > 0) {
+        $(this).parent().children(".cloneRemove").show();
+    }
 
 });
 
 $(document).on('click', '.cloneRemove', function ($this) {
-   
-     $(this).parent().remove();
+    cloneCounter--;
+    var oCloneId = $(this).parent().attr('id');
+    $('#' + oCloneId + 'Rows:nth-last-child(1)').remove();
+    if ($('#' + oCloneId + 'Rows').length == 0) {
+        $(this).hide();
+    }
+});
+$(document).ready(function () {
+    $('.cloneRemove').hide();
+});
 
-});
-$(document).ready(function(){
-    $(".contactAddressC .cloneRemove").hide();
-});
 $(document).on('click', '.editAdd', function ($this) {
     var editId = $(this).parent().attr('id');
     var parentInputs = document.getElementById(editId);
