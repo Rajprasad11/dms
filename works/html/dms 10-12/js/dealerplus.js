45d67e63331870d6bb6ce629ba7d09fcb04b6d5d
vanignanavel <kalaivani@falconnect.in>
2016-12-26 18:06:36 +0530

    new file added

--- works/html/dms 10-12/js/dealerplus.js
+++ works/html/dms 10-12/js/dealerplus.js
@@ -48,3 +48,27 @@ $(document).on('click', '.editAdd', function ($this) {
                $(this).parent(".dropdown").ToggleClass("open");
 //               $(this).children(".dropdown-menu").slideToggle();
             });
+$(document).ready(function () {
+//Input Numbers
+$('.data-number').keydown(function (e) {
+	    // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+             // Allow: Ctrl+A, Command+A
+
+            (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) || 
+             // Allow: home, end, left, right, down, up
+            (e.keyCode >= 35 && e.keyCode <= 40)) {
+
+                 // let it happen, don't do anything
+                 return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
+});
+$.validate();
+$(document).on('click', '.btn-primary', function ($this) {
+	$.validate();
+});
