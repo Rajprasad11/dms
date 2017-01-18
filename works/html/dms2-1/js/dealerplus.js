59cbfe4a9079c3100920dd8e8ab818af5eca5cdd
NandhiniAshokkumar <nandhini@falconnect.in>
2017-01-18 11:19:10 +0530

    date picker changes

--- works/html/dms2-1/js/dealerplus.js
+++ works/html/dms2-1/js/dealerplus.js
@@ -1,13 +1,19 @@
 $(document).ready(function () {
-                $("#search-slide").slideUp(0);
-            });
+    $("#search-slide").slideUp(0);
+});
 $(function () {
-    $('.date').datetimepicker();
+    $('.date').datetimepicker({
+        autoclose: true,
+        format: "yyyy-mm-dd",
+        startView: "month",
+        minView: "month",
+        maxView: "decade"
+    });
 });
 
-$('input').bind("cut copy paste",function(e) {
-          e.preventDefault();
-      });
+$('input').bind("cut copy paste", function (e) {
+    e.preventDefault();
+});
 /*Field Set Cloning on clicking add button. Button class should be defined as cloneAdd inside the node that has to cloned */
 var cloneCounter = 0;
 $(document).on('click', '.cloneAdd', function ($this) {
@@ -27,7 +33,7 @@ $(document).on('click', '.cloneAdd', function ($this) {
 $(document).on('click', '.cloneRemove', function ($this) {
     cloneCounter--;
     var oCloneId = $(this).parents(".cloneSet").attr('id');
-    $('#'+oCloneId).remove();
+    $('#' + oCloneId).remove();
 });
 $(document).ready(function () {
     $('.cloneRemove').hide();
@@ -43,65 +49,65 @@ $(document).on('click', '.editAdd', function ($this) {
 });
 
 
-            $(".search-click").click(function () {
-               $("#search-slide").slideToggle();
-            });
-            $(".dropdown-toggle").click(function(){
-               $(this).parent(".dropdown").ToggleClass("open");
+$(".search-click").click(function () {
+    $("#search-slide").slideToggle();
+});
+$(".dropdown-toggle").click(function () {
+    $(this).parent(".dropdown").ToggleClass("open");
 //               $(this).children(".dropdown-menu").slideToggle();
-            });
+});
 $(document).ready(function () {
 //Input Numbers
-$('.data-number').keydown(function (e) {
-	    // Allow: backspace, delete, tab, escape, enter and .
+    $('.data-number').keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
         if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 ||
-             // Allow: Ctrl+A, Command+A
+                // Allow: Ctrl+A, Command+A
 
-            (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) || 
-             // Allow: home, end, left, right, down, up
-            (e.keyCode >= 35 && e.keyCode <= 40)) {
+                        (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+                        // Allow: home, end, left, right, down, up
+                                (e.keyCode >= 35 && e.keyCode <= 40)) {
 
-                 // let it happen, don't do anything
-                 return;
-        }
-        // Ensure that it is a number and stop the keypress
-        if ((e.keyCode === 32 || e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-            e.preventDefault();
-        }
-    });
+                    // let it happen, don't do anything
+                    return;
+                }
+                // Ensure that it is a number and stop the keypress
+                if ((e.keyCode === 32 || e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+                    e.preventDefault();
+                }
+            });
 
-$('.data-name').keydown(function (e) {
-	    // Allow: backspace, delete, tab, escape, enter and .
+    $('.data-name').keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
         if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 32]) !== -1 ||
-             // Allow: Ctrl+A, Command+A
+                // Allow: Ctrl+A, Command+A
 
-            (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) || 
-             // Allow: home, end, left, right, down, up
-            (e.keyCode >= 35 && e.keyCode <= 40)) {
+                        (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+                        // Allow: home, end, left, right, down, up
+                                (e.keyCode >= 35 && e.keyCode <= 40)) {
 
-                 // let it happen, don't do anything
-                 return;
-        }
-        // Ensure that it is a number and stop the keypress
-        if (( e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105) && (e.keyCode < 65 || e.keyCode > 90)) {
-            e.preventDefault();
-        }
-    });
+                    // let it happen, don't do anything
+                    return;
+                }
+                // Ensure that it is a number and stop the keypress
+                if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105) && (e.keyCode < 65 || e.keyCode > 90)) {
+                    e.preventDefault();
+                }
+            });
 });
 $.validate();
 $(document).on('click', '.btn-primary', function ($this) {
-	$.validate();
+    $.validate();
 });
 $(document).on('click', '.aria-validate', function ($this) {
-	 $.validate({
-                     onError : function($form) {
-                     return false;
-    },
-    onSuccess : function($form) {
-        console.log("test");
-      $("form .aria-primary").click();
-	 }
-	 });
-	
-	
+    $.validate({
+        onError: function ($form) {
+            return false;
+        },
+        onSuccess: function ($form) {
+            console.log("test");
+            $("form .aria-primary").click();
+        }
+    });
+
+
 });
