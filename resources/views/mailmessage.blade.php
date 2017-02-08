fdcf7ea2e22ae75af18959822acc158db5790512
vanignanavel <kalaivani@falconnect.in>
2017-02-08 14:52:27 +0530

    unwanted files removed

--- resources/views/mailmessage.blade.php
+++
@@ -1,5 +0,0 @@
-
-This is auto generated mail for register authentication!!!<br/>
-
-please click the following link for register authentication...<br/>
-<a href="password_activation.blade.php">Password creation link..</a>
